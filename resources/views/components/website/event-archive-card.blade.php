a0e24e5742aa98da8ff8fec8391b64161a86ec22
Sdhaked <60962994+Sdhaked@users.noreply.github.com>
2026-08-22 21:32:29 +0530

    changes

---
+++ resources/views/components/website/event-archive-card.blade.php
@@ -0,0 +1,39 @@
+@props(['event'])
+
+@php
+    $eventUrl = route('website.events.show', $event->slug);
+    $imageAltText = $event->featured_image_alt_text ?: $event->title;
+@endphp
+
+<div {{ $attributes->merge(['class' => 'event-archive-card']) }}>
+    <a href="{{ $eventUrl }}" class="img-holder">
+        <img src="{{ asset('storage/' . $event->featured_image) }}"
+            alt="{{ $imageAltText }}" loading="lazy" decoding="async" />
+    </a>
+
+    <div class="text-holder">
+        <a href="{{ $eventUrl }}">
+            <h3 class="title">{{ $event->title }}</h3>
+        </a>
+        <p class="date-time">
+            <i class="fa-solid fa-calendar-days i-mr"></i>
+            <time datetime="{{ $event->from_date->format('Y-m-d') }}">{{ $event->from_date->format('M j, Y') }}</time>
+            @if ($event->to_date)
+                <span>
+                    -
+                    <time datetime="{{ $event->to_date->format('Y-m-d') }}">{{ $event->to_date->format('M j, Y') }}</time>
+                </span>
+            @endif
+        </p>
+        <p class="date-time">
+            <i class="fa-solid fa-clock i-mr"></i>
+            <time>{{ $event->from_time->format('g:i A') }}</time>
+            @if ($event->to_time)
+                <span>
+                    TO
+                    <time>{{ $event->to_time->format('g:i A') }}</time>
+                </span>
+            @endif
+        </p>
+    </div>
+</div>
